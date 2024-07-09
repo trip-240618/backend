90d067a699b12e3d275e6fdebd486ba6cb3d40cb
taehoon <xogns2134@gmail.com>
2024-07-09 22:11:22 +0900

    feat: websocket 기능 추가 & 메세지 전송 엔드포인트 생성

---
+++ src/main/java/com/ll/trip/domain/chat/chat/ChatController.java
@@ -0,0 +1,23 @@
+package com.ll.trip.domain.chat.chat;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+	private final SimpMessagingTemplate messagingTemplate;
+	@PostMapping("/chat/write")
+	@Operation(summary = "메세지 전송")
+	public ResponseEntity<?> write(
+		@RequestBody final String content) {
+		messagingTemplate.convertAndSend("/topic/chat", content);
+		return ResponseEntity.ok("전송 성공");
+	}
+}
