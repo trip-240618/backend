b991030ff41618bd707957955f0faf536c74d692
taehoon <xogns2134@gmail.com>
2024-09-10 18:10:12 +0900

    fix: refreshToken entity 수정

--- src/main/java/com/ll/trip/domain/alarm/alarm/config/SchedulerConfiguration.java
+++ src/main/java/com/ll/trip/domain/alarm/alarm/config/SchedulerConfiguration.java
@@ -1,6 +1,5 @@
 package com.ll.trip.domain.alarm.alarm.config;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ll.trip.domain.alarm.alarm.service.AlarmService;
@@ -13,10 +12,5 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SchedulerConfiguration {
 	private final AlarmService alarmService;
-
-	@Scheduled(fixedDelay = 60000)
-	public void sendAlarm() {
-		alarmService.sendAlarm();
-	}
 
 }
