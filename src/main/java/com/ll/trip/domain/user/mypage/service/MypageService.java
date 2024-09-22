b0cd0161d44fbabfd5027fa4a3d4d939e37c34d9
taehoon <xogns2134@gmail.com>
2024-09-22 16:50:14 +0900

    feat: 알림설정 엔티티 생성

--- src/main/java/com/ll/trip/domain/user/mypage/service/MypageService.java
+++ src/main/java/com/ll/trip/domain/user/mypage/service/MypageService.java
@@ -1,4 +1,18 @@
 package com.ll.trip.domain.user.mypage.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ll.trip.domain.user.mypage.entity.NotificationConfig;
+import com.ll.trip.domain.user.user.entity.UserEntity;
+
+@Service
+@Transactional(readOnly = true)
 public class MypageService {
+
+	@Transactional
+	public void createNotificationConfig(UserEntity user) {
+		NotificationConfig config = NotificationConfig.builder()
+			.user(user).build();
+	}
 }
