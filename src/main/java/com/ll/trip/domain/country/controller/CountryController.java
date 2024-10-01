f514d5557262bd1647f5733eefcf50a2f3d0e2d8
taehoon <xogns2134@gmail.com>
2024-10-02 01:04:41 +0900

    docs: swagger 설명 추가

--- src/main/java/com/ll/trip/domain/country/controller/CountryController.java
+++ src/main/java/com/ll/trip/domain/country/controller/CountryController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ll.trip.domain.country.entity.Country;
 import com.ll.trip.domain.country.service.CountryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,7 +22,10 @@ public class CountryController {
 	private final CountryService countryService;
 
 	@GetMapping("/country/search")
-
+	@Operation(summary = "나라 검색")
+	@ApiResponse(responseCode = "200", description = "db에서 country필드에 있는 나라이름으로 검색", content = {
+		@Content(mediaType = "application/json",
+			schema = @Schema(implementation = Country.class))})
 	public ResponseEntity<?> searchCountry(
 		@RequestParam String countryName
 	){
