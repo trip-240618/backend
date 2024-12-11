06f10bca02dd5c71339d52b92bb39f09f5eec4d7
taehoon <xogns2134@gmail.com>
2024-12-11 16:16:44 +0900

    Feat: 알림설정 생성자

--- src/main/java/com/ll/trip/domain/notification/notification/dto/NotificationConfigDto.java
+++ src/main/java/com/ll/trip/domain/notification/notification/dto/NotificationConfigDto.java
@@ -5,18 +5,20 @@ import com.ll.trip.domain.notification.notification.entity.NotificationConfig;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotificationConfigDto {
 	@Schema(description = "일정 알림")
-	private final boolean activePlan;
+	private boolean activePlan;
 
 	@Schema(description = "좋아요, 댓글 알림")
-	private final boolean activeLikeReply;
+	private boolean activeLikeReply;
 
 	@Schema(description = "마케팅 알림")
-	private final boolean activeMarketing;
+	private boolean activeMarketing;
 
 	public NotificationConfigDto(NotificationConfig config) {
 		this.activePlan = config.isActivePlanNotification();
