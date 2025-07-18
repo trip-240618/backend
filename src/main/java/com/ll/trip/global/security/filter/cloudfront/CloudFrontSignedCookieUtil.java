8cb4ccbe07a15020ca7a639217be1bad0a41d17e
pth2134 <xogns2134@gmail.com>
2025-07-18 18:54:05 +0900

    fix: 쿠키 오류 수정

--- src/main/java/com/ll/trip/global/security/filter/cloudfront/CloudFrontSignedCookieUtil.java
+++ src/main/java/com/ll/trip/global/security/filter/cloudfront/CloudFrontSignedCookieUtil.java
@@ -117,19 +117,11 @@ public class CloudFrontSignedCookieUtil {
 
     private Cookie makeSignedCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
-        cookie.setDomain(getRootDomain(cloudFrontDomain));
+        cookie.setDomain(cloudFrontDomain);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         return cookie;
-    }
-
-    private String getRootDomain(String domain) {
-        int dotIndex = domain.indexOf('.');
-        if (dotIndex != -1) {
-            return domain.substring(dotIndex + 1);
-        }
-        return domain;
     }
 
     public Map<String, String> extractCloudFrontCookies(Cookie[] cookies) {
